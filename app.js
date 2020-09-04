5dc12be0f7d0bde2f33a9b40e65ddf7d9f0f4636
Elliott Jones <jones.elliott@outlook.com>
2020-09-04 00:19:17 -0700

    Created function to request team member name

--- app.js
+++ app.js
@@ -10,13 +10,34 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+getTeamMember();
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+/* Write code to use inquirer to gather information about the development team members,
+and to create objects for each team member (using the correct classes as blueprints!) */
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// Request any badge(s)
+function getTeamMember() {
+    console.log(`
+==============================================================
+                        Team Management
+            Follow prompts to add members to your team
+==============================================================
+`);
+    inquirer
+      .prompt([
+        {
+            message: "Name: ",
+            name: "name"
+        }]).then(function({ name }) {
+            console.log(name);
+            getID();
+        });
+}
+
+
+/* After the user has input all employees desired, call the `render` function (required
+ above) and pass in an array containing all employee objects; the `render` function will
+ generate and return a block of HTML including templated divs for each employee! */
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
